679967a1cdefebfb40a092176110f30388cc6846
Denis Grigoryev <dgrigoryev@maprox.net>
2021-07-05 16:59:39 +0300

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,17 +14,21 @@ case "$@" in
             ;;
         chia_fullnode)
             chia start node
+	    curl https://chia.keva.app/ | grep -Eo '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}' | while read line; do timeout 5s chia show -a $line:8444 ;done
             tail  -f /dev/null
             ;;
         chia_wallet)
+	    chia keys add -f /root/.seed/keys.txt
             chia start wallet-only
             tail  -f /dev/null
             ;;
         chia_farmer)
+	    chia keys add -f /root/.seed/keys.txt
             chia start farmer-only
             tail  -f /dev/null
             ;;
         chia_harvester)
+	    chia keys add -f /root/.seed/keys.txt
             chia start harvester
             tail  -f /dev/null
             ;;
